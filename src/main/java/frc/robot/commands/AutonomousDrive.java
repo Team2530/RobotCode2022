d4de7dc44c926120ef75095cbf0bc7dd3bd2436a
Colten Kiehne <cokiehne@gmail.com>
2022-01-26 20:33:32 -0600

    Made it work (hopefully);

--- src/main/java/frc/robot/commands/AutonomousDrive.java
+++ src/main/java/frc/robot/commands/AutonomousDrive.java
@@ -6,59 +6,67 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.RobotContainer;
-import edu.wpi.first.math.trajectory.Trajectory.State;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.Timer;
 
 public class AutonomousDrive extends CommandBase {
   /** Creates a new AutonomousDrive. */
-  DriveTrain driveTrain; 
+  DriveTrain driveTrain;
+  Timer timer = new Timer();
+  double distanceTraveling = 0.0;
   AHRS ahrs = new AHRS();
-  Timer timer;
-  }
-  public AutonomousDrive(DriveTrain driveTrain) {
+  double distanceTraveled = 0.0;
+  double velocity = ahrs.getVelocityY();
+  double timeElapsed = 0;
+  String direction = "";
+  double forwardBack = 0;
+  double leftRight = 0;
+  double lastKnownTime = 0;
+  public AutonomousDrive(DriveTrain driveTrain, double distanceTraveling, String direction) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.driveTrain = driveTrain;
+    this.distanceTraveling = distanceTraveling;
+    this.direction = direction;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    // whatever direction is input, the robot (should) go that direction;
+    if (direction == "forward") {
+      forwardBack = 0.5;
+    }
+    if (direction == "back") {
+      forwardBack = -0.5;
+    }
+    if (direction == "left") {
+      leftRight = -0.5;
+    }
+    if (direction == "right") {
+      leftRight = 0.5;
+    }
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveTrain.singleJoystickDrive(0.5, 0, 0);
+    timer.start();
+    driveTrain.singleJoystickDrive(leftRight, forwardBack, 0);
+    velocity = ahrs.getVelocityY();
+    timeElapsed = timer.get();
+    // Distance = Rate * Time
+    distanceTraveled = distanceTraveled + Math.abs(velocity * (timeElapsed - lastKnownTime));
+    lastKnownTime = timer.get();
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
-  }
-  // distance should be in meters? Needs to be checked
-  public void driveForwardAutonomous(double distance){
-    double distanceTraveled = 0.0;
-    double velocity = ahrs.getVelocityY();
-    timer.reset();
-    timer.start();
-    double time = 0;
-    while(distance > distanceTraveled){
-       driveTrain.singleJoystickDrive(0, 0.5, 0);
-       velocity = ahrs.getVelocityY();
-       timeElapsed = timer.get();
-       // Distance = Rate * Time
-       distanceTraveled = distanceTraveled + abs(velocity * timeElapsed);
-       // reset and start time for next loop (There may be a better way to do this)
-       timer.reset();
-       timer.start();
-    }
-    // stop motors
-    driveTrain.singleJoystickDrive(0, 0 ,0);
   }
 }
