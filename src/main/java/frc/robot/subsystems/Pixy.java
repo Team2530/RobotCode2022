6f43420ecb8d794abd4ebb26d95b6e54ac01f8df
Lukas Singer <spelled.with.k@gmail.com>
2022-02-12 09:51:16 -0600

    Switch to subsystem and I2C

--- src/main/java/frc/robot/subsystems/Pixy.java
+++ src/main/java/frc/robot/subsystems/Pixy.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.subsystems;
 
 import java.util.ArrayList;
 
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import io.github.pseudoresonance.pixy2api.Pixy2;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
+import io.github.pseudoresonance.pixy2api.links.I2CLink;
 import io.github.pseudoresonance.pixy2api.links.SPILink;
 
 /**
@@ -46,7 +47,7 @@ public class Pixy extends TimedRobot {
   @Override
   public void robotInit() {
     // Initialize Pixy2.
-    pixy = Pixy2.createInstance(new SPILink());
+    pixy = Pixy2.createInstance(new I2CLink());
     int err = pixy.init(kPixySSPort);
     if (err < 0) {
       logPixyError(err);
