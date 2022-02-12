feb58d4112efdb70cf82becfc44b238d1da4aa22
Lukas Singer <spelled.with.k@gmail.com>
2022-02-12 09:45:26 -0600

    (Guru) Pixy refactor

--- src/main/java/frc/robot/commands/Pixy.java
+++ src/main/java/frc/robot/commands/Pixy.java
@@ -1,96 +1,147 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
-import frc.robot.subsystems.*;
-
-import java.io.Console;
 import java.util.ArrayList;
-import java.util.List;
 
-import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.pseudoresonance.pixy2api.Pixy2;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 import io.github.pseudoresonance.pixy2api.links.SPILink;
 
-public class Pixy extends SubsystemBase {
-  private final Pixy2 pixy;
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Pixy extends TimedRobot {
+  // SPI Slave Select pin (wired to CS0 on the practice chassix).
+  private static final int kPixySSPort = 0;
 
-  // ints for ball data
-  Block loadedblock;
-  double ballwidth;
-  double ballheight;
-  double ballx;
-  double ballY;
-  double ballAngle;
-  double ballAge;
-  double lastBlockCount;
+  private Pixy2 pixy;
 
-  public Pixy() {
-    pixy = Pixy2.createInstance(new SPILink());
-    pixy.init();
-    loadedblock = this.getBiggestBlock();
+  private boolean blockFound;
+  private int x, y, width, height;
+  private int fps;
+
+  /**
+   * Prints a error code from a pixy2 function.
+   * 
+   * @param errorCode
+   */
+  private void logPixyError(int errorCode) {
+    System.out.println("[PIXY ERROR AGHHHH] " + errorCode);
   }
 
-  public Block getBiggestBlock() {
-    // Finds blocks that match signature 1,
-    // finds the largest of them,
-    // then returns it in the original data-retrievable format
-
-    double blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25 /* limit on blocks to return */);
-    if (blockCount < 0) {
-      blockCount = lastBlockCount;
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Initialize Pixy2.
+    pixy = Pixy2.createInstance(new SPILink());
+    int err = pixy.init(kPixySSPort);
+    if (err < 0) {
+      logPixyError(err);
     }
-    lastBlockCount = blockCount;
-    SmartDashboard.putNumber("block count", blockCount);
 
-    if (blockCount <= 0) {
-      return null;
-    }
-    ArrayList<Block> blocks = pixy.getCCC().getBlockCache(); // Gets a list of all blocks found by the Pixy2
-    // blocks = pixy.getCCC().getBlocks(); // Gets a list of all blocks found by the
-    // Pixy2
+    // Print Pixy2 firmware version info.
+    System.out.println("[PIXY] " + pixy.getVersionInfo());
 
-    Block largestBlock = null;
-    for (Block block : blocks) { // Loops through all blocks and finds the widest one
-      if (block.getWidth() > largestBlock.getWidth() || block.getHeight() > largestBlock.getHeight()) {
-        largestBlock = block;
-      }
-    }
-    return largestBlock;
+    // Set LED and lamp to white.
+    pixy.setLamp((byte) 1, (byte) 1);
+    pixy.setLED(0, 255, 255);
+
+    // Put the alliance SmartDashboard variable.
+    SmartDashboard.putBoolean("Pixy/alliance", false);
   }
 
   @Override
-  public void periodic() {
+  public void robotPeriodic() {
+  }
 
-    // Gets Pixy data and stores in variables
-    loadedblock = this.getBiggestBlock();
-    System.out.println(loadedblock);
-    if (loadedblock != null) {
-      ballwidth = loadedblock.getWidth();
-      ballheight = loadedblock.getHeight();
-      ballx = loadedblock.getX(); // location on screen for ... maybe own angle calculations
-      ballY = loadedblock.getY();
-      ballAngle = loadedblock.getAngle(); // built in angle returning function
-      System.out.println("ballwidth : " + ballwidth + "; ballHeight : " + ballheight + "; ballx : " + ballx
-          + "; bally : " + ballY + "; ballangle : " + ballAngle);
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    // Error codes returned from pixy2 functions.
+    int err;
+
+    fps = pixy.getFPS();
+
+    // Get which alliance (true for blue, false for red) the robot is playing on,
+    // and set the signature of the pixy accordingly. As of now, signature 1 is for
+    // the blue cargo, signature 2 is for the red cargo.
+    byte signature = SmartDashboard.getBoolean("Pixy/alliance", false) ? Pixy2CCC.CCC_SIG1 : Pixy2CCC.CCC_SIG2;
+
+    // First argument is set to true to stop the PIXY_RESULT_BUSY error.
+    err = pixy.getCCC().getBlocks(true, signature, 25);
+    if (err <= 0) {
+      blockFound = false;
+
+      if (err < 0)
+        logPixyError(err);
+    } else {
+      blockFound = true;
+
+      // Get list of blocks.
+      ArrayList<Block> blocks = pixy.getCCC().getBlockCache();
+      Block largestBlock = null;
+
+      // Loop through blocks to find the one with the largest width.
+      for (Block block : blocks) {
+        if (largestBlock == null) {
+          largestBlock = block;
+        } else if (block.getWidth() > largestBlock.getWidth()) {
+          largestBlock = block;
+        }
+      }
+
+      x = largestBlock.getX();
+      y = largestBlock.getY();
+      width = largestBlock.getWidth();
+      height = largestBlock.getHeight();
     }
+
+    // Log everything to SmartDashboard
+    SmartDashboard.putBoolean("Pixy/Found Block", blockFound);
+    SmartDashboard.putNumber("Pixy/x", (double) x);
+    SmartDashboard.putNumber("Pixy/y", (double) y);
+    SmartDashboard.putNumber("Pixy/width", (double) width);
+    SmartDashboard.putNumber("Pixy/height", (double) height);
+    SmartDashboard.putNumber("Pixy/fps", (double) fps);
   }
 
-  public double getX() {
-    return ballx;
+  @Override
+  public void teleopInit() {
   }
 
-  public double getHeight() {
-    return ballwidth;
+  @Override
+  public void teleopPeriodic() {
   }
 
-  public double getWidth() {
-    return ballheight;
+  @Override
+  public void disabledInit() {
   }
 
-  public double getArea() {
-    return ballwidth * ballheight;
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
   }
 }
