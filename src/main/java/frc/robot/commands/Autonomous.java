1588e38f8e5b8d04eb55247ed176a69ff56bf24f
Lukas Singer <spelled.with.k@gmail.com>
2022-02-12 13:41:30 -0600

    Add sequential action support

--- src/main/java/frc/robot/commands/Autonomous.java
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -5,8 +5,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.AutonomousDrive;
 import frc.robot.subsystems.DriveTrain;
+
 public class Autonomous extends CommandBase {
   /** Creates a new Autonomous. */
   DriveTrain driveTrain = new DriveTrain();
@@ -22,22 +24,25 @@ public class Autonomous extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    AutonomousDrive autoVroomVroom = new AutonomousDrive(driveTrain, 1.5, 1);
+    SequentialCommandGroup autoVroomVroom = new SequentialCommandGroup(
+        new AutonomousDrive(driveTrain, 1.5, 1)
+    // add more commands here
+    );
     autoVroomVroom.schedule();
-
-
-
 
     // at end of all autonomous, sToP
     perhaps = true;
   }
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
