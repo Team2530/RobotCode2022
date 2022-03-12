8a414b2266944ae4b68c61ebc1fc31f68b6c20e5
Lukas Singer <spelled.with.k@gmail.com>
2022-03-12 11:20:26 -0600

    Camera went bye-bye

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -46,11 +46,8 @@ public class RobotContainer {
   private final Battery m_battery = new Battery(m_ahrs, xbox);
   private final DriveTrain m_driveTrain = new DriveTrain(m_ahrs);
   private final Climber m_climber = new Climber();
-  private final USBCamera usbCamera = new USBCamera();
   private final Intake intake = new Intake();
   private final Chambers ballDetection = new Chambers(3);
-
- 
 
   // -------------------- Autonomous Commands -------------------- \\
   // insert autonomous commands here
