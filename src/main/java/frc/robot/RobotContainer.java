6ed905de8dedf13528da8448d5416ad553f6693e
SF <sdfreimuth2@gmail.com>
2022-03-21 19:30:14 -0500

    (Lukas) Fix everything maybe
    This reverts commit df5d682b5381175d8dce60b6f4913d23873610b8.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,12 +10,18 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.cscore.MjpegServer;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
@@ -27,31 +33,39 @@ import frc.robot.subsystems.*;
  * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-
-  // -------------------- Subsystems -------------------- \\
-
-  private final DriveTrain m_driveTrain = new DriveTrain();
-  private final Climber m_climber = new Climber();
-  private final USBCamera usbCamera = new USBCamera();
-  private final Intake intake = new Intake();
-  private final Chambers ballDetection = new Chambers(3);
-  private final Shooter shooter = new Shooter();
-
 
   // -------------------- Joysticks and Buttons -------------------- \\
   // Joysticks
   final Joystick stick1 = new Joystick(Constants.stickport1); // Creates a joystick on port 1
-  final Joystick stick2 = new Joystick(Constants.stickport2); // Creates a joystick on port 2
-
+  
   // Xbox Controller
   final XboxController xbox = new XboxController(Constants.xboxport);
+
+  // The robot's subsystems and commands are defined here...
+
+  // -------------------- Subsystems -------------------- \\
+
+  private final AHRS m_ahrs = new AHRS();
+  private final Battery m_battery = new Battery(m_ahrs, xbox);
+  private final DriveTrain m_driveTrain = new DriveTrain(m_ahrs);
+  private final Climber m_climber = new Climber();
+  // private final USBCamera usbCamera = new USBCamera();
+  private final PhotonVision vision = new PhotonVision();
+  private final Intake intake = new Intake();
+  private final Chambers ballDetection = new Chambers(3);
+  private final Shooter shooter = new Shooter();
 
   // -------------------- Autonomous Commands -------------------- \\
   // insert autonomous commands here
 
   // -------------------- Telop Commands -------------------- \\
   // insert teleop commands here
+
+  // -------------------- Global Toggles -------------------- \\
+  /** Whether or not autonomous/smart systems are disabled. */
+  private static boolean manualMode = false;
+  private static boolean boostMode = false;
+  private static boolean slowMode = false;
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -68,8 +82,38 @@ public class RobotContainer {
    * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    // Climber control
-    new JoystickButton(stick1, 4).whenPressed(() -> m_climber.setClimberSpeed(0.5))
+    // Button for driving at full speed
+    new JoystickButton(stick1, 1).whenPressed(() -> {
+      boostMode = true;
+    }).whenReleased(() -> {
+      boostMode = false;
+    });
+
+    // Button for driving slowly
+    new JoystickButton(stick1, 2).whenPressed(() -> {
+      slowMode = true;
+    }).whenReleased(() -> {
+      slowMode = false;
+    });
+
+    // Disable rotation (not yet in this branch)
+
+    // Zero navX rotation
+    new JoystickButton(stick1, 4).whenPressed(() -> m_driveTrain.reset());
+
+    // Velocity retention (not yet in this branch)
+
+    // Button for disabling autonomous/smart functions
+    new JoystickButton(stick1, 6).whenPressed(() -> {
+      manualMode = true;
+    }).whenReleased(() -> {
+      manualMode = false;
+    });
+
+    // Climber control (RB for full power and LB for low power)
+    new JoystickButton(xbox, 6).whenPressed(() -> m_climber.setClimberSpeed(1.0))
+        .whenReleased(() -> m_climber.setClimberSpeed(0));
+    new JoystickButton(xbox, 5).whenPressed(() -> m_climber.setClimberSpeed(0.1))
         .whenReleased(() -> m_climber.setClimberSpeed(0));
     new JoystickButton(xbox, 5).whenPressed(() -> m_climber.setClimberSpeed(0.3))
         .whenReleased(() -> m_climber.setClimberSpeed(0));
@@ -106,31 +150,28 @@ public class RobotContainer {
     new JoystickButton(xbox, 2).whenPressed(() -> intake.setIntakeMotorSpeed(0, 0.75))
         .whenReleased(() -> intake.setIntakeMotorSpeed(0, 0));
 
-    // Upper intake up and also shooter (X button)
-    new JoystickButton(xbox, 3).whenPressed(
-      new ParallelCommandGroup(
-        new InstantCommand(() -> {
-          intake.setIntakeMotorSpeed(1, -0.75);
-        }),
-        new InstantCommand(() -> {
-          shooter.setShooterSpeed(1);
-        })
-      )
-    )
-      .whenReleased(
-        new ParallelCommandGroup(
-          new InstantCommand(() -> {
-            intake.setIntakeMotorSpeed(1, 0);
-          }),
-          new InstantCommand(() -> {
-            shooter.setShooterSpeed(0);
-          })
-        )
-      );
+    // Lower intake up (A button)
+    new JoystickButton(xbox, 1).whenPressed(() -> intake.setIntakeMotorSpeed(0, -0.75))
+        .whenReleased(() -> intake.setIntakeMotorSpeed(0, 0));
 
-    // new JoystickButton(stick1, 1).whenPressed(() ->
-    // m_driveTrain.driveStraight(0.5))
-    // .whenReleased(() -> m_driveTrain.driveStraight(0));
+    // Lower intake down (B button)
+    new JoystickButton(xbox, 2).whenPressed(() -> intake.setIntakeMotorSpeed(0, 0.75))
+        .whenReleased(() -> intake.setIntakeMotorSpeed(0, 0));
+  }
+
+  /** Returns whether or not the robot is driving at full speed. */
+  public static boolean getBoostMode() {
+    return boostMode;
+  }
+
+  /** Returns whether or not the robot is driving in slow mode. */
+  public static boolean getSlowMode() {
+    return slowMode;
+  }
+
+  /** Returns whether or not autonomous/smart systems are disabled. */
+  public static boolean getManualMode() {
+    return manualMode;
   }
 
   /**
