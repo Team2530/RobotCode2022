930e52ee6f1cafa02063ca4dcf97fd7ed49fd62e
Clarissa Peterson <Clarissa.m.peterson@gmail.com>
2022-03-23 20:53:44 -0500

    (Luke) WIP - Made a universal axis converter

---
+++ src/main/java/frc/robot/libraries/AxisConverter.java
@@ -0,0 +1,26 @@
+public class AxisConverter {
+    public static double[] convertFromNAVX(double[] inp) { // if it is easier change [] param to 3 seperate
+
+    }
+    public static double[] convertFromJoystick(double[] inp) {
+/**
+ * forward = -y
+ * backward = +y
+ * left = -x
+ * right = +x
+ * CCW = -z
+ * CW = +z
+ */
+    }
+    public static double[] convertToCartesian(double[] inp) {
+/**
+ * y is flipped compared to joystick
+ */
+    }
+    public static double[] convertFromMecanum(double[] inp) {
+
+    }
+    public static double[] convertToXYZ(double[] inp) {
+
+    }
+}
