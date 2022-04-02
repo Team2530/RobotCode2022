2fd8554ac01e1c011100f10bea019b7004bda0df
NifleySnifley <sailzforjunkmailz@gmail.com>
2022-04-02 14:41:59 -0500

    Feedback panel display control

---
+++ src/main/java/frc/robot/subsystems/FeedbackPanel.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+
+import frc.robot.InCANDevice;
+
+public class FeedbackPanel extends InCANDevice {
+    public static enum PanelMode {
+        Status(0),
+        Boot(1),
+        Climb(2);
+
+        private final byte value;
+
+        PanelMode(final int v) {
+            value = new Integer(v).byteValue();
+        }
+
+        public byte getValue() {
+            return value;
+        }
+    };
+
+    public FeedbackPanel(int deviceNum) {
+        super(deviceNum);
+        this.apiID = 28 << 4;
+    }
+
+    @Override
+    public void periodic() {
+    }
+
+    public void setDisplayMode(PanelMode mode) {
+        sendData(new byte[] { mode.getValue() });
+    }
+}
